070afa0122b66414959abbf56fc16bf5b6cdab1e
Alijalal-Dev <aj0546006@gmail.com>
2025-02-07 21:46:54 +0100

    Modify files 7 in py and html

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-"""HBNB Flask application."""
-from flask import Flask, render_template
-from models import storage
-from models.state import State
+"""
+start Flask application
+"""
 
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Render a template to display a list of states."""
-    states = storage.all(State)
-    return render_template("7-states_list.html", states=states)
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """
-    Close the database connection after each request.
-
-    Args:
-        exception: Any exception that occurred during the request handling.
-    """
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
